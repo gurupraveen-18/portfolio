1c5661eada97db57f24782e6b1cd7a12256cc1a5
gurupraveen-18 <121022113+gurupraveen-18@users.noreply.github.com>
2024-07-25 14:02:17 +0530

    Update style.js

--- style.js
+++ style.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing",{
-		strings: ["Aspiring Developer ", "Coding Enthusiast", " Full Stack Developer"],
+		strings: [" FrontEnd Developer ", " Youtuber ", " Content Creator "],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Aspiring Developer", "Coding Enthusiast", "Full Stack Developer"],
+        strings: [" FrontEnd Developer ", " Youtuber ", " Content Creator "],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
